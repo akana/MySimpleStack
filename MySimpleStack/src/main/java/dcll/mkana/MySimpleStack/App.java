9762f8868ac72036487ae3434f169fabd84ab92a
21307425 <21307425@207-07.mig.snap.fsi>
2014-02-26 09:37:28 +0100

    q

--- MySimpleStack/src/main/java/dcll/mkana/MySimpleStack/App.java
+++ MySimpleStack/src/main/java/dcll/mkana/MySimpleStack/App.java
@@ -7,5 +7,6 @@ package dcll.mkana.MySimpleStack;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("kana is the best");
 	}
 }
